5114189a4ce2a58d5bc1e11638e11bb071b41b66
Leonel Ramirez <leferami@espol.edu.ec>
2022-08-26 08:18:12 -0500

    prueba unitaria

--- src/app/service/pokemon.service.spec.ts
+++ src/app/service/pokemon.service.spec.ts
@@ -9,19 +9,24 @@ import { PokemonService } from './pokemon.service';
 
 describe('PokemonService', () => {
   let service: PokemonService;
+  let serviceGet: PokemonService;
   let httpClientSpy: { post: jasmine.Spy };
+  let httpClientSpyGet: { get: jasmine.Spy };
   beforeEach(() => {
     TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(PokemonService);
+    serviceGet = TestBed.inject(PokemonService);
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+    httpClientSpyGet = jasmine.createSpyObj('HttpClient', ['get']);
     service = new PokemonService(httpClientSpy as any);
+    serviceGet = new PokemonService(httpClientSpyGet as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('Debe retornar objeto cuando la creacion sea correcta', (done: DoneFn) => {
+  it('Debe retornar objeto cuando la creacion de un pokemon sea correcta', (done: DoneFn) => {
     const mockPokemon = {
       name: 'Raikiu',
       image:
@@ -51,4 +56,37 @@ describe('PokemonService', () => {
       }
     });
   });
+
+  it('Debe retornar una lista de objeto de pokemon cuando se consulte por get', (done: DoneFn) => {
+    const mockPokemon = 1;
+    const mpockResultPokemon = [
+      {
+          "id": 445,
+          "name": "pikachu",
+          "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+          "attack": 66,
+          "defense": 85,
+          "hp": 100,
+          "type": "Pokemon",
+          "id_author": 2
+      },
+      {
+          "id": 940,
+          "name": "Raikiu",
+          "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/026.png",
+          "attack": 83,
+          "defense": 64,
+          "hp": 100,
+          "type": "golden",
+          "id_author": 2
+      }];
+    httpClientSpyGet.get.and.returnValue(of(mpockResultPokemon));
+    serviceGet.getPokemonAutor(mockPokemon).subscribe({
+      next: (resp) =>{
+        expect(resp).toEqual(mpockResultPokemon)
+        done()
+      }
+    });
+  });
+
 });
